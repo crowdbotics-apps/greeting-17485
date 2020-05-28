7f07c72e8fdd2e64cbd07e6b9f83d5b31ccb7406
Crowdbotics <team@crowdbotics.com>
2020-05-28 01:28:32 +0000

    Update models

--- backend/greeting_17485/wsgi.py
+++ backend/greeting_17485/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'greeting_17485.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "greeting_17485.settings")
 
 application = get_wsgi_application()
